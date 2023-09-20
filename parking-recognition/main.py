4d3c57a762ec7947d71fde30795237dd04886dc1
Kieran Miller <kieran.miller@emerson.com>
2023-09-20 14:11:47 -0400

    Add basic image recognition with boxing

--- parking-recognition/main.py
+++ parking-recognition/main.py
@@ -1,9 +1,14 @@
 import numpy as np
 import cv2 as cv
-cap = cv.VideoCapture(0)
+cap = cv.VideoCapture(0, cv.CAP_DSHOW)
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
+cur_frame, prev_frame = None, None
+motion_detected = False
+
+mog = cv.createBackgroundSubtractorMOG2()
+
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -12,9 +17,30 @@ while True:
         print("Can't receive frame (stream end?). Exiting ...")
         break
     # Our operations on the frame come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    cur_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    cur_frame = cv.GaussianBlur(cur_frame, (21, 21), 0)
+    if prev_frame is None:
+        prev_frame = cur_frame
+        continue
+    fgmask = mog.apply(cur_frame)
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))
+    fgmask = cv.erode(fgmask, kernel, iterations=1)
+    fgmask = cv.dilate(fgmask, kernel, iterations=1)
+
+    contours, hierarchy = cv.findContours(fgmask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        # Ignore small contours
+        if cv.contourArea(contour) < 1000:
+            continue
+
+        # Draw bounding box around contour
+        x, y, w, h = cv.boundingRect(contour)
+        cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv.imshow('Motion Detection', frame)
     # Display the resulting frame
-    cv.imshow('frame', gray)
+
     if cv.waitKey(1) == ord('q'):
         break
 # When everything done, release the capture
