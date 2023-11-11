d45f069c3a02a10f2a9da63ce43c41773ac2800d
Kieran Miller <kieran.miller@emerson.com>
2023-11-11 18:49:27 -0500

    Use Python 2 syntax

--- pi/update.py
+++ pi/update.py
@@ -4,10 +4,10 @@ SERVER_URL = 'http://172.31.56.243:5000'
 
 def send_increase():
     """Sends a request to the server to increase parking lot capacity."""
-    response = requests.post(f'{SERVER_URL}/increase', json={'amount': 1})
+    response = requests.post('{}/increase'.format(SERVER_URL), json={'amount': 1})
     return response.ok
 
 def send_decrease():
     """Sends a request to the server to decrease parking lot capacity."""
-    response = requests.post(f'{SERVER_URL}/decrease', json={'amount': 1})
+    response = requests.post('{}/decrease'.format(SERVER_URL), json={'amount': 1})
     return response.ok
