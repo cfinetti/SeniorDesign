5a82301b498a088539775106f3a1e31fa5b8eeea
Kieran Miller <kieran.miller133@gmail.com>
2023-11-11 18:57:05 -0500

    Update detect.py opencv for Python2

--- pi/detect.py
+++ pi/detect.py
@@ -4,31 +4,30 @@ from update import send_decrease, send_increase
 
 def car_entered():
     if send_increase():
-        print("Increased capacity successfully.")
+        print "Increased capacity successfully."
     else:
-        print("Failed to increase capacity.")
+        print "Failed to increase capacity."
 
 def car_exited():
     if send_decrease():
-        print("Decreased capacity successfully.")
+        print "Decreased capacity successfully."
     else:
-        print("Failed to decrease capacity or capacity is already zero.")
+        print "Failed to decrease capacity or capacity is already zero."
 
-cap = cv.VideoCapture(0, cv.CAP_DSHOW)
+cap = cv.VideoCapture(0)
 if not cap.isOpened():
-    print("Cannot open camera")
+    print "Cannot open camera"
     exit()
 cur_frame, prev_frame = None, None
-motion_detected = False
 
-mog = cv.createBackgroundSubtractorMOG2()
+mog = cv.BackgroundSubtractorMOG2()
 
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
     # if frame is read correctly ret is True
     if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
+        print "Can't receive frame (stream end?). Exiting ..."
         break
     # Our operations on the frame come here
     cur_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
@@ -41,7 +40,8 @@ while True:
     fgmask = cv.erode(fgmask, kernel, iterations=1)
     fgmask = cv.dilate(fgmask, kernel, iterations=1)
 
-    contours, hierarchy = cv.findContours(fgmask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    # Find contours
+    contours, _ = cv.findContours(fgmask.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
         # Ignore small contours
@@ -55,7 +55,7 @@ while True:
     cv.imshow('Motion Detection', frame)
     # Display the resulting frame
 
-    if cv.waitKey(1) == ord('q'):
+    if cv.waitKey(1) & 0xFF == ord('q'):
         break
 # When everything done, release the capture
 cap.release()
