68c99a9c7e57b5eb83ecd2e6b4abba0a8b5f1de3
Kieran Miller <kieran.miller133@gmail.com>
2023-11-18 21:00:24 -0500

    Match protocols for websocket connection

--- app/static/js/websocket.js
+++ app/static/js/websocket.js
@@ -1,7 +1,7 @@
 var socket;
 
 function initWebSocket() {
-    socket = io.connect('http://' + document.domain + ':' + location.port);
+    socket = io.connect('//' + document.domain + ':' + location.port);
     socket.on('parking_update', function(data) {
         updateParkingLotMarkers(data);
     });
