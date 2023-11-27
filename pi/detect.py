138b5cb6b3d6c73d7db04440a139fa15e0996139
Kieran Miller <kieran.miller133@gmail.com>
2023-11-27 15:38:00 -0500

    YOLOv8 detection with onnx

--- pi/detect.py
+++ pi/detect.py
@@ -1,62 +1,29 @@
-import numpy as np
-import cv2 as cv
-from update import send_decrease, send_increase
+import cv2
 
-def car_entered():
-    if send_decrease():
-        print("Decreased availability successfully.")
-    else:
-        print("Failed to decrease availability.")
+from yolov8 import YOLOv8
 
-def car_exited():
-    if send_decrease():
-        print("Increased availability successfully.")
-    else:
-        print("Failed to increase availability.")
+# Initialize the webcam
+cap = cv2.VideoCapture(0)
 
-cap = cv.VideoCapture(0, cv.CAP_DSHOW)
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
-cur_frame, prev_frame = None, None
-motion_detected = False
+# Initialize YOLOv7 object detector
+model_path = "models/trained.onnx"
+yolov8_detector = YOLOv8(model_path, conf_thres=0.5, iou_thres=0.5)
 
-mog = cv.createBackgroundSubtractorMOG2()
+cv2.namedWindow("Detected Objects", cv2.WINDOW_NORMAL)
+while cap.isOpened():
 
-while True:
-    # Capture frame-by-frame
+    # Read frame from the video
     ret, frame = cap.read()
-    # if frame is read correctly ret is True
+
     if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
         break
-    # Our operations on the frame come here
-    cur_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    cur_frame = cv.GaussianBlur(cur_frame, (21, 21), 0)
-    if prev_frame is None:
-        prev_frame = cur_frame
-        continue
-    fgmask = mog.apply(cur_frame)
-    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))
-    fgmask = cv.erode(fgmask, kernel, iterations=1)
-    fgmask = cv.dilate(fgmask, kernel, iterations=1)
 
-    contours, hierarchy = cv.findContours(fgmask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    # Update object localizer
+    boxes, scores, class_ids = yolov8_detector(frame)
 
-    for contour in contours:
-        # Ignore small contours
-        if cv.contourArea(contour) < 1000:
-            continue
+    combined_img = yolov8_detector.draw_detections(frame)
+    cv2.imshow("Detected Objects", combined_img)
 
-        # Draw bounding box around contour
-        x, y, w, h = cv.boundingRect(contour)
-        cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-    cv.imshow('Motion Detection', frame)
-    # Display the resulting frame
-
-    if cv.waitKey(1) == ord('q'):
+    # Press key q to stop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-# When everything done, release the capture
-cap.release()
-cv.destroyAllWindows()
